abf3d4b5ac86c81ff80057b01f6dc35707fbb3e4
llau08 <llau251@gmail.com>
2021-11-19 09:35:45 -0800

    need to fix button and scroll

--- src/components/OutroCards/OutroCards.js
+++ src/components/OutroCards/OutroCards.js
@@ -7,26 +7,32 @@ export default function OutroCards(){
     return(
         <div className="card">
         <article className="card__box">
-            <p className="card__txt">"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor."</p>
-            <div className="card__sub-box">
-                <img className="card__avatar" src={angelina} alt="avatar"/>
-                <p>Angelina, 25</p>
+            <div className="card__box--2">
+                <p className="card__txt">"I used to think that the crypto currency world was a man's world. Thank you for showing me otherwise."</p>
+                <div className="card__sub-box">
+                    <img className="card__avatar" src={angelina} alt="avatar"/>
+                    <p>Angelina, 25</p>
+                </div>
             </div>
         </article>
 
         <article className="card__box">
-            <p className="card__txt">"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor."</p>
-            <div className="card__sub-box">
-                <img className="card__avatar" src={charles} alt="avatar" />
-                <p>Charles, 32</p>
+            <div className="card__box--2">
+                <p className="card__txt">"Once you start it's not that intimidating. My whole family uses Robinhood."</p>
+                <div className="card__sub-box">
+                    <img className="card__avatar" src={charles} alt="avatar" />
+                    <p>Charles, 32</p>
+                </div>
             </div>
         </article>
 
         <article className="card__box">
-            <p className="card__txt">"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor."</p>
-            <div className="card__sub-box">
-                <img className="card__avatar" src={david} alt="avatar"/>
-                <p>David, 60</p>
+            <div className="card__box--2">
+                <p className="card__txt">"Accessible and friendly support staff. Can't be beat on customer service."</p>
+                <div className="card__sub-box">
+                    <img className="card__avatar" src={david} alt="avatar"/>
+                    <p>David, 60</p>
+                </div>
             </div>
         </article>
         </div>
